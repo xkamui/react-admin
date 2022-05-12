14d18ec8af228b257833276b39e52620b6f7fa0a
xkamui <x_kamui@msn.com>
2022-05-12 14:59:01 +0200

    Setting up topbar template

---
+++ src/components/topbar/TopNotif.jsx
@@ -0,0 +1,13 @@
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+
+export default function TopNotif(props) {
+  return (
+    <div className="topNotif">
+      <Tooltip title={props.name}>
+        <IconButton>{props.icon}</IconButton>
+      </Tooltip>
+      {props.notif ? <span className="number">{props.notif}</span> : ""}
+    </div>
+  );
+}
